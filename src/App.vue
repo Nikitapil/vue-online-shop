7c5e583e4f067f9c0a5ca46b0a518b376891e242
Nikitapil <nikitapiliavets@gmail.com>
2023-12-12 13:05:38 +0300

    add Header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from '@/components/Header.vue';
+</script>
 
 <template>
-  <div class="bg-red-400">Hello</div>
+  <div class="w-4/5 m-auto bg-white h-screen rounded-xl shadow-xl mt-10">
+    <Header />
+  </div>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="flex justify-between border-b border-b-slate-300 px-8 py-6">
+    <div class="flex items-center gap-4">
+      <img
+        class="w-10"
+        src="/logo.png"
+        alt="Logo "
+      />
+
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <p class="text-slate-500">Best sneakers shop</p>
+      </div>
+    </div>
+
+    <ul class="flex gap-8 items-center">
+      <li class="flex gap-3 items-center text-slate-500 cursor-pointer hover:text-black">
+        <img
+          src="/cart.svg"
+          alt="cart"
+        />
+        <b>1205руб</b>
+      </li>
+
+      <li class="flex gap-3 items-center text-slate-500 cursor-pointer hover:text-black">
+        <img
+          src="/heart.svg"
+          alt="cart"
+        />
+        <span>Bookmarks</span>
+      </li>
+
+      <li class="flex gap-3 items-center text-slate-500 cursor-pointer hover:text-black">
+        <img
+          src="/profile.svg"
+          alt="cart"
+        />
+        <span>Profile</span>
+      </li>
+    </ul>
+  </header>
+</template>
