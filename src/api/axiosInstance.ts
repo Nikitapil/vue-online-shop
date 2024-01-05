039408c66f7c127690a87f36c8e40e01fe150f5c
Nikitapil <nikitapiliavets@gmail.com>
2024-01-05 21:13:29 +0300

    refactor http-client template

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { getTokenFromStorage, setTokenToStorage } from '@/helpers/token-helpers';
-import { Api } from '@/api/swagger/Api';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
