ff2a668d03cafc5527e99f608f0ab98bdc9f7925
Nikitapil <nikitapiliavets@gmail.com>
2024-03-09 13:16:05 +0300

    add pagination for ProductsReviewsList

--- src/modules/product-page/components/ProductReviewsList/ProductReviewsList.vue
+++ src/modules/product-page/components/ProductReviewsList/ProductReviewsList.vue
@@ -1,17 +1,45 @@
-<script setut lang="ts">
-import ProductsPagination from '../../../../components/products/ProductsPagination.vue';
+<script setup lang="ts">
+import ProductsPagination from '@/components/products/ProductsPagination.vue';
 import type { ProductReviewReturnDto } from '@/api/swagger/data-contracts';
 import { ref } from 'vue';
 
 const props = defineProps<{
   reviews: ProductReviewReturnDto[];
   totalCount: number;
+  isLoading: boolean;
+}>();
+
+const emit = defineEmits<{
+  loadProducts: [{ page: number; limit: number }];
 }>();
 
 const page = ref(1);
 const limitValue = ref('10');
+
+const onPaginate = () => {
+  emit('loadProducts', { page: page.value, limit: +limitValue.value });
+};
+
+const onChangePage = (newPage: number) => {
+  page.value = newPage;
+  onPaginate();
+};
+
+const onChangeLimit = () => {
+  page.value = 1;
+  onPaginate();
+};
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <ProductsPagination
+      v-model:limit-value="limitValue"
+      :current-page="page"
+      :total-products-count="props.totalCount"
+      :is-loading="props.isLoading"
+      @change-page="onChangePage"
+      @change-limit="onChangeLimit"
+    />
+  </div>
 </template>
