bdec52ffcb54cf0081fc13bc5577c024355d3b29
Nikitapil <nikitapiliavets@gmail.com>
2024-03-18 21:00:42 +0300

    fix icon on product page

--- src/modules/product-page/pages/ProductPage.vue
+++ src/modules/product-page/pages/ProductPage.vue
@@ -64,7 +64,7 @@ onMounted(() => {
         />
         <IconButton
           v-if="product.canEdit"
-          :icon="['bx:edit', 1]"
+          :icon="'bx:edit'"
           color="#918aa0"
           @click="router.push({ name: ERoutesName.EDIT_PRODUCT, params: { id: store.product.id } })"
         />
