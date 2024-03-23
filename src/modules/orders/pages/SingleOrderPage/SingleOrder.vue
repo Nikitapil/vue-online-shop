a8c335a85e7ad78bd7a28ae7339eb65ed0a540f0
Nikitapil <nikitapiliavets@gmail.com>
2024-03-23 13:45:20 +0300

    Add requisites block to SingleOrder page

--- src/modules/orders/pages/SingleOrderPage/SingleOrder.vue
+++ src/modules/orders/pages/SingleOrderPage/SingleOrder.vue
@@ -23,8 +23,15 @@ onMounted(() => {
     <EmptyStateCentered v-else-if="!store.order">
       <p class="text-2xl font-bold">Order not found</p>
     </EmptyStateCentered>
+
     <div v-else>
-      <h2>Order id {{ store.order.id }}</h2>
+      <h2 class="text-xl w-fit border-b pb-1 mb-5">Order id: {{ store.order.id }}</h2>
+      <div class="w-fit border-b pb-1">
+        <h3 class="text-lg">Requisites:</h3>
+        <p><span class="font-bold">Address:</span> {{ store.order.address }}</p>
+        <p><span class="font-bold">Phone:</span> {{ store.order.phone }}</p>
+      </div>
+
       <OrderProduct
         v-for="product in store.order.productsInOrder"
         :key="product.id"
