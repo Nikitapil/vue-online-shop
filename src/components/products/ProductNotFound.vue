6979f417e819a58674386d4db2cfdf78fac29fca
Nikitapil <nikitapiliavets@gmail.com>
2024-05-09 20:27:18 +0300

    refactor ProductNotFoundComponent

--- src/components/products/ProductNotFound.vue
+++ src/components/products/ProductNotFound.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue/dist/iconify.js';
+
 import EmptyStateCentered from '../ui/EmptyStateCentered.vue';
 </script>
 
 <template>
   <EmptyStateCentered class="flex-col gap-2">
-    <h2 class="text-2xl font-bold">Product not found</h2>
+    <h2 class="text-2xl font-bold">{{ $t('product_not_found') }}</h2>
     <Icon
       icon="nonicons:not-found-16"
       color="gray"
