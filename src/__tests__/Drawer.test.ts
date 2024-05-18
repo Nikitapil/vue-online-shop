6018f6c97be958345d19f13ed7d4d08cc1ea0b67
Nikitapil <nikitapiliavets@gmail.com>
2024-05-18 21:12:40 +0300

    add Drawer tests

---
+++ src/__tests__/Drawer.test.ts
@@ -0,0 +1,91 @@
+import { mount } from '@vue/test-utils';
+import Drawer from '../components/ui/Drawer.vue';
+import IconButton from '../components/ui/IconButton.vue';
+
+describe('Drawer tests', () => {
+  test('Should be close if modelValue is false', () => {
+    const wrapper = mount(Drawer, {
+      props: {
+        title: 'Darwer test',
+        modelValue: false
+      }
+    });
+
+    const overlay = wrapper.find('[data-testid="drawer-overlay"]');
+    const content = wrapper.find('[data-testid="drawer-content"]');
+
+    expect(overlay.exists()).toBe(false);
+    expect(content.exists()).toBe(false);
+  });
+
+  test('Should be opened if modelValue is false', () => {
+    const wrapper = mount(Drawer, {
+      props: {
+        title: 'Darwer test',
+        modelValue: true
+      }
+    });
+
+    const overlay = wrapper.find('[data-testid="drawer-overlay"]');
+    const content = wrapper.find('[data-testid="drawer-content"]');
+
+    expect(overlay.exists()).toBe(true);
+    expect(content.exists()).toBe(true);
+  });
+
+  test('Should be correctly applied title', () => {
+    const title = 'Darwer test';
+    const wrapper = mount(Drawer, {
+      props: {
+        title,
+        modelValue: true
+      }
+    });
+
+    const titleNode = wrapper.find('[data-testid="drawer-title"]');
+
+    expect(titleNode.text()).toBe(title);
+  });
+
+  test('Should close by click on overlay', async () => {
+    const title = 'Darwer test';
+    const wrapper = mount(Drawer, {
+      props: {
+        title,
+        modelValue: true,
+        'onUpdate:modelValue': (e: boolean) => wrapper.setProps({ modelValue: e })
+      }
+    });
+
+    let overlay = wrapper.find('[data-testid="drawer-overlay"]');
+
+    await overlay.trigger('click');
+
+    overlay = wrapper.find('[data-testid="drawer-overlay"]');
+    const content = wrapper.find('[data-testid="drawer-content"]');
+
+    expect(overlay.exists()).toBe(false);
+    expect(content.exists()).toBe(false);
+  });
+
+  test('Should close by click on arrow', async () => {
+    const title = 'Darwer test';
+    const wrapper = mount(Drawer, {
+      props: {
+        title,
+        modelValue: true,
+        'onUpdate:modelValue': (e: boolean) => wrapper.setProps({ modelValue: e })
+      }
+    });
+
+    const arrowBtn = wrapper.findComponent(IconButton);
+
+    await arrowBtn.trigger('click');
+
+    const overlay = wrapper.find('[data-testid="drawer-overlay"]');
+    const content = wrapper.find('[data-testid="drawer-content"]');
+
+    expect(overlay.exists()).toBe(false);
+    expect(content.exists()).toBe(false);
+  });
+});
--- src/components/ui/Drawer.vue
+++ src/components/ui/Drawer.vue
@@ -14,6 +14,7 @@ const close = () => (isOpen.value = false);
   <div
     v-if="isOpen"
     class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-50"
+    data-testid="drawer-overlay"
     @click="close"
   ></div>
 
@@ -26,13 +27,19 @@ const close = () => (isOpen.value = false);
     <div
       v-if="isOpen"
       class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8 flex flex-col"
+      data-testid="drawer-content"
     >
       <div class="flex items-center gap-5 mb-5">
         <IconButton
           icon="formkit:arrowleft"
           @click="close"
         />
-        <h2 class="text-2xl font-bold">{{ title }}</h2>
+        <h2
+          class="text-2xl font-bold"
+          data-testid="drawer-title"
+        >
+          {{ title }}
+        </h2>
       </div>
 
       <slot />
