78093140159c5449c8366d78cb6a9b556bd6d818
Nikitapil <nikitapiliavets@gmail.com>
2024-05-20 21:05:50 +0300

    refactor search input component

--- src/components/ui/SearchInput.vue
+++ src/components/ui/SearchInput.vue
@@ -24,12 +24,13 @@ defineEmits<{
       color="#E4E4E4"
       icon="fluent:search-12-filled"
     />
+
     <input
       v-model="value"
       class="border rounded-md py-2 pl-10 h-11 pr-4 outline-none focus:border-gray-400 disabled:bg-gray-100 disabled:cursor-not-allowed"
       type="text"
       :disabled="disabled"
-      placeholder="Search"
+      :placeholder="$t('search')"
       @input="$emit('input', value)"
     />
   </div>
