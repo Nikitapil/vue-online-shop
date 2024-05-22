0090bd2042a3673f66403f2ea29a8d8783130ef9
Nikitapil <nikitapiliavets@gmail.com>
2024-05-22 23:16:14 +0300

    add useProduct tests

---
+++ src/__tests__/mocks/product-mocks.ts
@@ -0,0 +1,25 @@
+export const product = {
+  id: 'product',
+  name: 'product-name',
+  description: 'product-description',
+  imageUrl: '/qwe',
+  price: 123,
+  priceWithDiscount: 123,
+  categoryId: '1234',
+  createdAt: '2023-11-29T17:09:20.867Z',
+  updatedAt: '2023-11-29T17:09:20.867Z',
+  category: {
+    id: '5678',
+    name: 'category',
+    createdAt: '2023-11-29T17:09:20.867Z',
+    updatedAt: '2023-11-29T17:09:20.867Z'
+  },
+  isInFavorites: false,
+  rating: null,
+  canAddReview: true,
+  canEdit: true,
+  canDelete: true,
+  canAddToFavourites: true,
+  canEditProductDiscount: true,
+  discount: null
+};
---
+++ src/__tests__/useProduct.test.ts
@@ -0,0 +1,19 @@
+import { ref } from 'vue';
+import { useProduct } from '../composables/useProduct';
+import { product } from "./mocks/product-mocks";
+
+describe('useProduct test', () => {
+  test('should return null if null value passed', () => {
+    const product = ref(null);
+    const result = useProduct(product);
+
+    expect(result.value).toBeNull();
+  });
+
+  test('should return product with correct url', () => {
+    const productValue = ref(product);
+    const result = useProduct(productValue);
+
+    expect(result.value.imageUrl).toBe('http://localhost:5000/qwe');
+  });
+});
