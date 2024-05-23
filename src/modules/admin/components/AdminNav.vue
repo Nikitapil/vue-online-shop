1ab15e7190fa93734b51ce0f9ebf4ebf75560e78
Nikitapil <nikitapiliavets@gmail.com>
2024-05-23 22:03:00 +0300

    add finance_settings link localization

--- src/modules/admin/components/AdminNav.vue
+++ src/modules/admin/components/AdminNav.vue
@@ -7,7 +7,7 @@ import { ERoutesName } from '@/router';
   <div class="border-r border-r-black">
     <LinkWithIcon
       icon="arcticons:home-finance"
-      label="Finance Settings"
+      :label="$t('finance_settings')"
       class="px-4 py-2 hover:bg-slate-200"
       active-class-name="bg-slate-100"
       :to="{ name: ERoutesName.FINANCE_SETTINGS }"
