9ebbe38cccd41004140757e6f86316d4067ee186
Nikitapil <nikitapiliavets@gmail.com>
2024-05-25 18:07:59 +0300

    refactor Admin page component

--- src/modules/admin/pages/Admin.vue
+++ src/modules/admin/pages/Admin.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+import { UserReturnDtoRolesEnum } from '@/api/swagger/data-contracts';
+
 import AdminNav from '@/modules/admin/components/AdminNav.vue';
 import AuthProtected from '@/modules/auth/components/AuthProtected.vue';
-import { UserReturnDtoRolesEnum } from '@/api/swagger/data-contracts';
 </script>
 
 <template>
   <AuthProtected :admitted-roles="[UserReturnDtoRolesEnum.ADMIN]">
     <div class="flex">
       <AdminNav />
+
       <div class="px-8 flex-1">
         <RouterView />
       </div>
