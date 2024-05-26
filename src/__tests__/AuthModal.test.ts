2b7bb83b3ed6415a280b2dc905cc3679ac7a028e
Nikitapil <nikitapiliavets@gmail.com>
2024-05-26 21:36:51 +0300

    add AuthModal tests

---
+++ src/__tests__/AuthModal.test.ts
@@ -0,0 +1,55 @@
+import { mount } from '@vue/test-utils';
+import { createTestingPinia } from '@pinia/testing';
+import { validation } from '../plugins/validation';
+
+import { EAuthModalTypes } from '../domain/components';
+import { i18n } from '../plugins/i18n';
+
+import AuthModal from '../modules/auth/components/AuthModal.vue';
+import AuthForm from '../modules/auth/components/AuthForm.vue';
+
+describe('AuthModal tests', () => {
+  test('should not show AuthModal if modelValue is false', () => {
+    const wrapper = mount(AuthModal, {
+      props: {
+        modelValue: false
+      }
+    });
+
+    const authFrom = wrapper.findComponent(AuthForm);
+
+    expect(authFrom.exists()).toBe(false);
+  });
+
+  test('should show correct AuthForm for login', () => {
+    const wrapper = mount(AuthModal, {
+      global: {
+        plugins: [i18n, createTestingPinia(), validation]
+      },
+      props: {
+        modelValue: true,
+        initialType: EAuthModalTypes.LOGIN
+      }
+    });
+
+    const authFrom = wrapper.findComponent(AuthForm);
+
+    expect(authFrom.props('initialType')).toBe(EAuthModalTypes.LOGIN);
+  });
+
+  test('should show correct AuthForm for register', () => {
+    const wrapper = mount(AuthModal, {
+      global: {
+        plugins: [i18n, createTestingPinia(), validation]
+      },
+      props: {
+        modelValue: true,
+        initialType: EAuthModalTypes.REGISTER
+      }
+    });
+
+    const authFrom = wrapper.findComponent(AuthForm);
+
+    expect(authFrom.props('initialType')).toBe(EAuthModalTypes.REGISTER);
+  });
+});
