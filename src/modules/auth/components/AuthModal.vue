d3e0b6db93581bf8b1ea850946efbbc12fe0b314
Nikitapil <nikitapiliavets@gmail.com>
2024-05-26 21:24:51 +0300

    AuthModal tests

--- src/modules/auth/components/AuthModal.vue
+++ src/modules/auth/components/AuthModal.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
+import { EAuthModalTypes } from '@/domain/components';
+
 import Modal from '@/components/ui/Modal.vue';
 import AuthForm from '@/modules/auth/components/AuthForm.vue';
+
 const isOpen = defineModel<boolean>();
 
 withDefaults(
   defineProps<{
-    initialType?: 'login' | 'register';
+    initialType?: EAuthModalTypes;
   }>(),
   {
-    initialType: 'login'
+    initialType: EAuthModalTypes.LOGIN
   }
 );
 </script>
