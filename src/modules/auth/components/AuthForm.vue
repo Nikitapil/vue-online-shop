b855c53a2cf68135106ac978b6bf20d4af912ab9
Nikitapil <nikitapiliavets@gmail.com>
2024-05-26 20:06:24 +0300

    refactor AuthForm

--- src/modules/auth/components/AuthForm.vue
+++ src/modules/auth/components/AuthForm.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
-import AppInput from '@/components/ui/AppInput/AppInput.vue';
-import AppButton from '@/components/ui/AppButton.vue';
 import { useForm } from 'vee-validate';
+import { useI18n } from 'vue-i18n';
+
+import { EAuthModalTypes } from '@/domain/components';
+
 import { useAuthStore } from '@/modules/auth/authStore';
+
+import AppButton from '@/components/ui/AppButton.vue';
+import AppInput from '@/components/ui/AppInput/AppInput.vue';
+
+const { t } = useI18n();
 
 const props = withDefaults(
   defineProps<{
-    initialType?: 'login' | 'register';
+    initialType?: EAuthModalTypes;
   }>(),
   {
-    initialType: 'login'
+    initialType: EAuthModalTypes.LOGIN
   }
 );
 
@@ -33,23 +40,23 @@ const form = ref({
   phone: ''
 });
 
-const isRegisterType = computed(() => type.value === 'register');
+const isRegisterType = computed(() => type.value === EAuthModalTypes.REGISTER);
 
-const title = computed(() => (isRegisterType.value ? 'Sign up' : 'Log in'));
+const title = computed(() => (isRegisterType.value ? t('sign_up') : t('log_in')));
 
-const swapButtonText = computed(() =>
-  isRegisterType.value ? 'Already have an account? Log in.' : 'Not registered yet? Sign up.'
-);
+const swapButtonText = computed(() => (isRegisterType.value ? t('already_registered_text') : t('not_registered_text')));
 
 const passwordMatchValidation = () => {
   if (form.value.password !== form.value.passwordRepeat) {
-    return 'Passwords should be equal';
+    return t('passwords_should_be_equal');
   }
+
   return true;
 };
 
 const register = async () => {
   const { email, password, name, address, phone } = form.value;
+
   await authStore.register({
     email,
     password,
@@ -61,11 +68,13 @@ const register = async () => {
 
 const login = async () => {
   const { email, password } = form.value;
+
   await authStore.login({ email, password });
 };
 
 const onSubmit = async () => {
   const validationResult = await validate();
+
   if (validationResult.valid) {
     if (isRegisterType.value) {
       await register();
@@ -73,12 +82,13 @@ const onSubmit = async () => {
       await login();
     }
   }
+
   if (authStore.isAuthenticated) {
     emit('success');
   }
 };
 
-const swapForm = () => (type.value = isRegisterType.value ? 'login' : 'register');
+const swapForm = () => (type.value = isRegisterType.value ? EAuthModalTypes.LOGIN : EAuthModalTypes.REGISTER);
 </script>
 
 <template>
@@ -87,23 +97,25 @@ const swapForm = () => (type.value = isRegisterType.value ? 'login' : 'register'
     @submit.prevent="onSubmit"
   >
     <h1 class="text-2xl font-bold">{{ title }}</h1>
+
     <AppInput
       id="email"
       v-model="form.email"
       name="email"
       label="Email:"
-      placeholder="Your email"
       rules="required|email"
+      :placeholder="t('your_email')"
       :disabled="authStore.isAuthLoading"
     />
+
     <AppInput
       id="password"
       v-model="form.password"
       name="password"
       type="password"
-      label="Password:"
-      placeholder="Your password"
       rules="required|minLength:8"
+      :placeholder="t('your_password')"
+      :label="`${t('password')}:`"
       :disabled="authStore.isAuthLoading"
     />
 
@@ -113,37 +125,41 @@ const swapForm = () => (type.value = isRegisterType.value ? 'login' : 'register'
         v-model="form.passwordRepeat"
         name="password-repeat"
         type="password"
-        label="Repeat password:"
-        placeholder="Your password"
+        :label="`${t('repeat_password')}:`"
+        :placeholder="t('your_password')"
         :rules="passwordMatchValidation"
         :disabled="authStore.isAuthLoading"
       />
+
       <AppInput
         id="name"
         v-model="form.name"
         name="name"
-        label="Name:"
-        placeholder="Your name"
         rules="required"
+        :placeholder="t('your_name')"
+        :label="`${t('name')}:`"
         :disabled="authStore.isAuthLoading"
       />
+
       <AppInput
         id="address"
         v-model="form.address"
         name="address"
-        label="Address:"
-        placeholder="Your address"
+        :placeholder="t('your_address')"
+        :label="`${t('address')}:`"
         :disabled="authStore.isAuthLoading"
       />
+
       <AppInput
         id="phone"
         v-model="form.phone"
         name="phone"
-        label="Phone:"
-        placeholder="Your phone"
+        :label="`${t('phone')}:`"
+        :placeholder="t('your_phone')"
         :disabled="authStore.isAuthLoading"
       />
     </template>
+
     <div class="self-end">
       <AppButton
         appearance="transparent"
@@ -153,6 +169,7 @@ const swapForm = () => (type.value = isRegisterType.value ? 'login' : 'register'
         {{ swapButtonText }}
       </AppButton>
     </div>
+
     <AppButton
       appearance="success"
       type="submit"
