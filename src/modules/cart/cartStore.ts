0df65f7961c58907d12c7a2fa94f4b75d5711acf
Nikitapil <nikitapiliavets@gmail.com>
2024-05-29 20:41:35 +0300

    refactor cartStore

--- src/modules/cart/cartStore.ts
+++ src/modules/cart/cartStore.ts
@@ -1,75 +1,60 @@
-import { api } from '@/api/apiInstance';
-import type { CartReturnDto, CreateOrderDto } from '@/api/swagger/data-contracts';
+import { computed, ref } from 'vue';
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
-import { toast } from 'vue3-toastify';
+
+import { api } from '@/api/apiInstance';
+
+import type { CartReturnDto, CreateOrderDto } from '@/api/swagger/data-contracts';
+
+import { useApiMethod } from '@/api/useApiMethod';
 
 export const useCartStore = defineStore('cart', () => {
   const cart = ref<CartReturnDto | null>(null);
-  const isCartLoading = ref(false);
-  const isChangeInCartCountInProgress = ref(false);
-  const isCreateOrderInProgress = ref(false);
+
+  const { call: getCart, isLoading: isCartLoading } = useApiMethod(api.getCart);
+  const { call: addToCartApi, isLoading: isAddToCartLoading } = useApiMethod(api.addToCart);
+  const { call: removeFromCartApi, isLoading: isRemoveFromCartLoading } = useApiMethod(api.removeFromCart);
+  const { call: createOrderApi, isLoading: isCreateOrderInProgress } = useApiMethod(api.createOrder);
+  const { call: clearCartApi, isLoading: isClearCartInProgress } = useApiMethod(api.clearCart);
+
+  const isChangeInCartCountInProgress = computed(
+    () => isAddToCartLoading.value || isRemoveFromCartLoading.value || isClearCartInProgress.value
+  );
 
   const loadCart = async () => {
-    try {
-      isCartLoading.value = true;
-      cart.value = await api.getCart();
-    } catch (e: any) {
-      toast.error(e?.response?.data?.message || 'Error');
-    } finally {
-      isCartLoading.value = false;
-    }
+    cart.value = await getCart();
   };
 
   const addToCart = async (productId: string) => {
-    try {
-      isChangeInCartCountInProgress.value = true;
-      cart.value = await api.addToCart({ productId });
+    const response = await addToCartApi({ productId });
+    if (response) {
+      cart.value = response;
       return true;
-    } catch (e: any) {
-      toast.error(e?.response?.data?.message || 'Error');
+    } else {
       return false;
-    } finally {
-      isChangeInCartCountInProgress.value = false;
     }
   };
 
   const removeFromCart = async (productInCartId: string) => {
-    try {
-      isChangeInCartCountInProgress.value = true;
-      cart.value = await api.removeFromCart({ productInCartId });
-    } catch (e: any) {
-      toast.error(e?.response?.data?.message || 'Error');
-    } finally {
-      isChangeInCartCountInProgress.value = false;
+    const response = await removeFromCartApi({ productInCartId });
+    if (response) {
+      cart.value = response;
+      return true;
+    } else {
+      return false;
     }
   };
 
   const createOrder = async (orderData: CreateOrderDto) => {
-    try {
-      isCreateOrderInProgress.value = true;
-      const { cart: cartResponse, order } = await api.createOrder(orderData);
+    const response = await createOrderApi(orderData);
+    if (response) {
+      const { cart: cartResponse, order } = response;
       cart.value = cartResponse;
       return order.id;
-    } catch (e: any) {
-      toast.error(e?.response?.data?.message || 'Error');
-      return null;
-    } finally {
-      isCreateOrderInProgress.value = false;
     }
   };
 
   const clearCart = async () => {
-    try {
-      isChangeInCartCountInProgress.value = true;
-      cart.value = await api.clearCart();
-      return true;
-    } catch (e: any) {
-      toast.error(e?.response?.data?.message || 'Error');
-      return false;
-    } finally {
-      isChangeInCartCountInProgress.value = false;
-    }
+    cart.value = await clearCartApi();
   };
 
   return {
