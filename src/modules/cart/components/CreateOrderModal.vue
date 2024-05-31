ec4118f1831c7ca58f7b33ed0d5f3a24118924bc
Nikitapil <nikitapiliavets@gmail.com>
2024-05-31 20:45:40 +0300

    refactor CreateOrderModal component

--- src/modules/cart/components/CreateOrderModal.vue
+++ src/modules/cart/components/CreateOrderModal.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useForm } from 'vee-validate';
+
+import { useAuthStore } from '@/modules/auth/authStore';
+
+import type { CreateOrderDto } from '@/api/swagger/data-contracts';
+
 import AppTextarea from '../../../components/ui/AppInput/AppTextarea.vue';
 import AppInput from '../../../components/ui/AppInput/AppInput.vue';
 import Modal from '../../../components/ui/Modal.vue';
 import AppButton from '@/components/ui/AppButton.vue';
-import { useAuthStore } from '@/modules/auth/authStore';
-import { ref } from 'vue';
-import { useForm } from 'vee-validate';
-import type { CreateOrderDto } from '@/api/swagger/data-contracts';
 
 const isOpened = defineModel<boolean>();
+
 const authStore = useAuthStore();
 
 const props = defineProps<{
@@ -27,6 +31,7 @@ const comment = ref('');
 
 const onSubmit = async () => {
   const validationResult = await validate();
+
   if (validationResult.valid) {
     emit('create', {
       address: address.value,
@@ -47,46 +52,51 @@ const onSubmit = async () => {
       class="flex flex-col gap-3"
       @submit.prevent="onSubmit"
     >
-      <h1 class="text-center text-2xl font-bold">Order creation</h1>
+      <h1 class="text-center text-2xl font-bold">{{ $t('order_creation') }}</h1>
+
       <AppInput
         id="address"
         v-model="address"
         name="address"
-        label="Delivery address"
-        placeholder="Enter delivery address"
         rules="required"
+        :label="$t('delivery_address')"
+        :placeholder="$t('enter_delivery_address')"
         :disabled="props.isLoading"
       />
+
       <AppInput
         id="phone"
         v-model="phone"
         name="phone"
-        label="Your contact phone"
-        placeholder="Enter contact phone"
         rules="required"
+        :placeholder="$t('enter_contact_phone')"
+        :label="$t('your_contact_phone')"
         :disabled="props.isLoading"
       />
+
       <AppTextarea
-        id="commet"
+        id="comment"
         v-model="comment"
-        label="Comment"
         name="comment"
-        placeholder="Enter comment for order"
+        :label="$t('comment')"
+        :placeholder="$t('enter_comment_for_order')"
         :disabled="props.isLoading"
       />
+
       <div class="flex gap-3 justify-end">
         <AppButton
           :disabled="props.isLoading"
           @click="isOpened = false"
         >
-          Cancel
+          {{ $t('cancel') }}
         </AppButton>
+
         <AppButton
           appearance="success"
           type="submit"
           :disabled="props.isLoading"
         >
-          Create
+          {{ $t('create') }}
         </AppButton>
       </div>
     </form>
