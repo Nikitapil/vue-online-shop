e03030db1a7b4643c454fcf309c13efea4510527
Nikitapil <nikitapiliavets@gmail.com>
2024-06-01 15:29:57 +0300

    refactor SingleDiscount page

--- src/modules/discounts/pages/SingleDiscount/SingleDiscount.vue
+++ src/modules/discounts/pages/SingleDiscount/SingleDiscount.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
 import { computed, onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
 import { useSingleDiscountStore } from '@/modules/discounts/pages/SingleDiscount/singleDiscountStore';
+
+import type { IDiscountParams } from '@/modules/discounts/types';
+
 import EmptyStateCentered from '@/components/ui/EmptyStateCentered.vue';
 import RoundedLoader from '@/components/ui/loaders/RoundedLoader.vue';
 import ProductList from '@/widgets/ProductList/components/ProductList.vue';
 import AppButton from '@/components/ui/AppButton.vue';
 import ConfirmModal from '@/components/ui/ConfirmModal.vue';
 import DiscountFormModal from '@/modules/discounts/components/DiscountFormModal.vue';
-import type { IDiscountParams } from '@/modules/discounts/types';
 
 const route = useRoute();
 
@@ -17,15 +20,15 @@ const store = useSingleDiscountStore();
 const isDeleteModalOpened = ref(false);
 const isEditModalOpened = ref(false);
 
-const editDiscount = async (params: IDiscountParams) => {
-  await store.editDiscount(params);
-  isEditModalOpened.value = false;
-};
-
 const discountInitialEditData = computed(() => ({
   name: store.discount?.name || '',
   percentage: store.discount?.percentage || 0
 }));
+
+const editDiscount = async (params: IDiscountParams) => {
+  await store.editDiscount(params);
+  isEditModalOpened.value = false;
+};
 
 onMounted(() => {
   store.init(route.params.id as string);
@@ -36,41 +39,49 @@ onMounted(() => {
   <EmptyStateCentered v-if="store.isDiscountLoading">
     <RoundedLoader />
   </EmptyStateCentered>
+
   <EmptyStateCentered v-else-if="!store.discount">
-    <p class="text-2xl font-bold">Discount not found</p>
+    <p class="text-2xl font-bold">{{ $t('discount_not_found') }}</p>
   </EmptyStateCentered>
+
   <div v-else>
     <div class="flex justify-center items-center gap-3">
       <h2 class="text-center text-xl font-bold">{{ store.discount.name }}</h2>
+
       <AppButton
         v-if="store.discount.canDelete"
         appearance="danger"
         @click="isDeleteModalOpened = true"
       >
-        Delete
+        {{ $t('delete') }}
       </AppButton>
+
       <AppButton
         v-if="store.discount.canEdit"
         appearance="primary"
         @click="isEditModalOpened = true"
       >
-        Edit
+        {{ $t('edit') }}
       </AppButton>
     </div>
-    <p class="mb-2">Discount percent: {{ store.discount.percentage }}</p>
+
+    <p class="mb-2">{{ $t('discount_percent') }}: {{ store.discount.percentage }}</p>
+
     <ProductList
       :is-loading="store.isDiscountLoading"
       :products="store.discount.products"
     />
+
     <ConfirmModal
       v-model="isDeleteModalOpened"
-      title="Do you want to delete this discount"
+      :title="$t('delete_discount_question')"
       :is-loading="store.isDiscountDeleteInProgress"
       @confirm="store.deleteDiscount"
     />
+
     <DiscountFormModal
       v-model="isEditModalOpened"
-      title="Edit discount"
+      :title="$t('edit_discount')"
       :is-loading="store.isEditDiscountInProgress"
       :initial-values="discountInitialEditData"
       @submit="editDiscount"
