c7aa3ee92f7f0eb8b9c276cf87afc7588ac51cce
Nikitapil <nikitapiliavets@gmail.com>
2024-06-01 14:35:39 +0300

    refactor Discounts page

--- src/modules/discounts/pages/Discounts/Discounts.vue
+++ src/modules/discounts/pages/Discounts/Discounts.vue
@@ -1,47 +1,54 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
 import { useDiscountsStore } from '@/modules/discounts/pages/Discounts/discountsStore';
-import EmptyStateCentered from '@/components/ui/EmptyStateCentered.vue';
+import { useAuthStore } from '@/modules/auth/authStore';
+
+import type { CreateDiscountDto } from '@/api/swagger/data-contracts';
+
+import DiscountFormModal from '@/modules/discounts/components/DiscountFormModal.vue';
+import AppButton from '@/components/ui/AppButton.vue';
 import RoundedLoader from '@/components/ui/loaders/RoundedLoader.vue';
 import DiscountCard from '@/modules/discounts/components/DiscountCard.vue';
-import { onMounted, ref } from 'vue';
-import AppButton from '@/components/ui/AppButton.vue';
-import { useAuthStore } from '@/modules/auth/authStore';
-import DiscountFormModal from '@/modules/discounts/components/DiscountFormModal.vue';
-import type { CreateDiscountDto } from '@/api/swagger/data-contracts';
+import EmptyStateCentered from '@/components/ui/EmptyStateCentered.vue';
 
 const store = useDiscountsStore();
 const authStore = useAuthStore();
 
 const isCreateModalShowed = ref(false);
 
-onMounted(() => {
-  store.getDiscounts();
-});
-
 const createDiscount = async (data: CreateDiscountDto) => {
   await store.createDiscount(data);
   isCreateModalShowed.value = false;
 };
+
+onMounted(() => {
+  store.getDiscounts();
+});
 </script>
 
 <template>
   <div>
     <div class="flex gap-2 items-center mb-3">
-      <h2 class="text-xl font-bold">Discounts</h2>
+      <h2 class="text-xl font-bold">{{ $t('discounts') }}</h2>
+
       <AppButton
         v-if="authStore.isAdmin"
         appearance="secondary"
         @click="isCreateModalShowed = true"
       >
-        Create discount
+        {{ $t('create_discount') }}
       </AppButton>
     </div>
+
     <EmptyStateCentered v-if="store.isDiscountsLoading">
       <RoundedLoader />
     </EmptyStateCentered>
+
     <EmptyStateCentered v-else-if="!store.discounts.length">
-      <p>No discounts at that moment</p>
+      <p>{{ $t('no_discounts_yet') }}</p>
     </EmptyStateCentered>
+
     <div class="flex flex-wrap gap-3">
       <DiscountCard
         v-for="discount in store.discounts"
@@ -50,9 +57,10 @@ const createDiscount = async (data: CreateDiscountDto) => {
       />
     </div>
   </div>
+
   <DiscountFormModal
     v-model="isCreateModalShowed"
-    title="Create discount"
+    :title="$t('create_discount')"
     :is-loading="store.isDiscountCreateInProgress"
     @submit="createDiscount"
   />
