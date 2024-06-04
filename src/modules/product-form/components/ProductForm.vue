7de8a08fdfafd1139a19c2a3abc6d9cfb089ba0e
Nikitapil <nikitapiliavets@gmail.com>
2024-06-04 21:14:19 +0300

    refactor ProductForm

--- src/modules/product-form/components/ProductForm.vue
+++ src/modules/product-form/components/ProductForm.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
-import AppInput from '@/components/ui/AppInput/AppInput.vue';
 import { computed, onMounted, ref } from 'vue';
-import CategoriesModal from '@/modules/categories/components/CategoriesModal.vue';
-import AppButton from '@/components/ui/AppButton.vue';
-import CategoriesSelect from '@/modules/categories/components/CategoriesSelect.vue';
 import { useForm } from 'vee-validate';
-import Uploader from '@/components/ui/Uploader.vue';
+import { useI18n } from 'vue-i18n';
+
 import type { CreateProductBody } from '@/api/swagger/data-contracts';
 import type { IProductForm } from '@/modules/product-form/types';
-import ContentSwitcher from '@/components/ui/ContentSwitcher/ContentSwitcher.vue';
 import type { ContentSwitcherOption } from '@/components/ui/ContentSwitcher/types';
+
+import ContentSwitcher from '@/components/ui/ContentSwitcher/ContentSwitcher.vue';
+import Uploader from '@/components/ui/Uploader.vue';
+import CategoriesSelect from '@/modules/categories/components/CategoriesSelect.vue';
+import AppButton from '@/components/ui/AppButton.vue';
+import CategoriesModal from '@/modules/categories/components/CategoriesModal.vue';
+import AppInput from '@/components/ui/AppInput/AppInput.vue';
 import ProductDetails from '@/components/products/ProductDetails.vue';
+import AppTextarea from '@/components/ui/AppInput/AppTextarea.vue';
+
+const { t } = useI18n();
 
 const { validate } = useForm();
 
@@ -29,17 +35,6 @@ const emit = defineEmits<{
   save: [CreateProductBody];
 }>();
 
-const switcherOptions: ContentSwitcherOption[] = [
-  {
-    name: 'Product form',
-    value: 'form'
-  },
-  {
-    name: 'Product preview',
-    value: 'preview'
-  }
-];
-
 const isCategoriesModalOpened = ref(false);
 const currentView = ref('form');
 
@@ -50,6 +45,17 @@ const product = ref<IProductForm>({
   price: '',
   image: null
 });
+
+const switcherOptions = computed<ContentSwitcherOption[]>(() => [
+  {
+    name: t('product_form'),
+    value: 'form'
+  },
+  {
+    name: t('product_preview'),
+    value: 'preview'
+  }
+]);
 
 const productReview = computed(() => ({
   ...product.value,
@@ -84,44 +90,50 @@ onMounted(() => {
       :options="switcherOptions"
     />
   </div>
+
   <ProductDetails
     v-if="currentView === 'preview'"
     :product="productReview"
   />
+
   <form
     v-else
     class="flex flex-col gap-3"
     @submit.prevent="submitHandler"
   >
     <h1 class="text-2xl font-bold text-center">{{ title }}</h1>
+
     <AppInput
       id="name"
       v-model="product.name"
       name="name"
-      placeholder="Title"
-      label="Product title"
       rules="required"
+      :placeholder="$t('title')"
+      :label="$t('product_title')"
       :disabled="isLoading"
     />
-    <AppInput
+
+    <AppTextarea
       id="description"
       v-model="product.description"
       name="description"
-      placeholder="Description"
-      label="Product description"
       rules="required"
+      :placeholder="$t('description')"
+      :label="$t('product_description')"
       :disabled="isLoading"
     />
+
     <AppInput
       id="price"
       v-model="product.price"
       name="price"
-      placeholder="Price"
-      label="Product price"
       rules="required"
       mask="numberMask"
+      :placeholder="$t('price')"
+      :label="$t('product_price')"
       :disabled="isLoading"
     />
+
     <div class="flex gap-3">
       <div class="flex-1">
         <CategoriesSelect
@@ -131,31 +143,35 @@ onMounted(() => {
           :full="true"
         />
       </div>
+
       <AppButton
         appearance="secondary"
         :disabled="isLoading"
         @click="isCategoriesModalOpened = true"
       >
-        Manage categories
+        {{ $t('manage_categories') }}
       </AppButton>
     </div>
+
     <Uploader
       id="image"
       v-model="product.image"
-      label="Upload product image"
       name="image"
       rules="requiredObj"
+      :label="$t('upload_product_image')"
       :disabled="isLoading"
     />
+
     <div class="self-end">
       <AppButton
         appearance="primary"
         type="submit"
         :disabled="isLoading"
       >
-        Save
+        {{ $t('save') }}
       </AppButton>
     </div>
   </form>
+
   <CategoriesModal v-model="isCategoriesModalOpened" />
 </template>
