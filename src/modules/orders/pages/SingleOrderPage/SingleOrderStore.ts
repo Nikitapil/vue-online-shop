f82182eda01deac28ca7b6b19675eb792abb59be
Nikitapil <nikitapiliavets@gmail.com>
2024-06-04 20:11:46 +0300

    refactor SingleOrderStore

--- src/modules/orders/pages/SingleOrderPage/SingleOrderStore.ts
+++ src/modules/orders/pages/SingleOrderPage/SingleOrderStore.ts
@@ -1,28 +1,25 @@
-import { api } from '@/api/apiInstance';
-import type { OrderReturnDto } from '@/api/swagger/data-contracts';
-import { defineStore } from 'pinia';
 import { computed, ref } from 'vue';
-import { toast } from 'vue3-toastify';
-import { type UpdateOrderStatusDto } from '../../../../api/swagger/data-contracts';
+import { defineStore } from 'pinia';
+
+import { useApiMethod } from '@/api/useApiMethod';
 import { getStatusColor } from '../../helpers/utils';
+
+import type { OrderReturnDto } from '@/api/swagger/data-contracts';
+import type { UpdateOrderStatusDto } from '@/api/swagger/data-contracts';
+
+import { api } from '@/api/apiInstance';
 
 export const useSingleOrderStore = defineStore('single-order', () => {
   const order = ref<OrderReturnDto | null>(null);
-  const isLoading = ref(false);
-  const isUpdateStatusInProgress = ref(false);
   const id = ref('');
+
+  const { call: getOrderById, isLoading } = useApiMethod(api.getOrderById);
+  const { call: updateOrderStatusApi, isLoading: isUpdateStatusInProgress } = useApiMethod(api.updateOrderStatus);
 
   const statusColor = computed(() => (order.value?.status ? getStatusColor(order.value?.status) : ''));
 
   const loadOrder = async () => {
-    try {
-      isLoading.value = true;
-      order.value = await api.getOrderById(id.value);
-    } catch (e: any) {
-      toast.error(e?.response?.data?.message || 'Error');
-    } finally {
-      isLoading.value = false;
-    }
+    order.value = await getOrderById(id.value);
   };
 
   const init = async (idFromRoute: string) => {
@@ -34,16 +31,8 @@ export const useSingleOrderStore = defineStore('single-order', () => {
     if (!order.value) {
       return;
     }
-
-    try {
-      isUpdateStatusInProgress.value = true;
-      await api.updateOrderStatus({ id: order.value.id, status, cancelReason });
-      await loadOrder();
-    } catch (e: any) {
-      toast.error(e?.response?.data?.message || 'Error');
-    } finally {
-      isUpdateStatusInProgress.value = false;
-    }
+    await updateOrderStatusApi({ id: order.value.id, status, cancelReason });
+    await loadOrder();
   };
 
   return { order, isLoading, isUpdateStatusInProgress, statusColor, init, updateOrderStatus };
