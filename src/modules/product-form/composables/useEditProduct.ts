5913bd02be70ad85fa307ed1222382719c2d4103
Nikitapil <nikitapiliavets@gmail.com>
2024-06-06 22:35:32 +0300

    refactor useEditProduct composition

--- src/modules/product-form/composables/useEditProduct.ts
+++ src/modules/product-form/composables/useEditProduct.ts
@@ -1,21 +1,15 @@
+import { useApiMethod } from '@/api/useApiMethod';
+
 import { api } from '@/api/apiInstance';
+
 import type { UpdateProductBody } from '@/api/swagger/data-contracts';
-import { ref } from 'vue';
-import { toast } from 'vue3-toastify';
 
 export const useEditProduct = () => {
-  const isLoading = ref(false);
+  const { call: editProductApi, isLoading } = useApiMethod(api.editProduct);
 
   const editProduct = async (data: UpdateProductBody) => {
-    isLoading.value = true;
-    try {
-      const product = await api.editProduct(data);
-      return product;
-    } catch (e: any) {
-      toast.error(e?.response?.data?.message || 'Error');
-    } finally {
-      isLoading.value = false;
-    }
+    const product = await editProductApi(data);
+    return product;
   };
 
   return {
