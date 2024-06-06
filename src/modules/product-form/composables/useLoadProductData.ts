7860630946b720f6b2038baa3a8d953b9ff68ee9
Nikitapil <nikitapiliavets@gmail.com>
2024-06-06 22:54:56 +0300

    refactor useLoadProductData

--- src/modules/product-form/composables/useLoadProductData.ts
+++ src/modules/product-form/composables/useLoadProductData.ts
@@ -1,24 +1,21 @@
-import { toast } from 'vue3-toastify';
-import { api } from '@/api/apiInstance';
-import type { ProductReturnDto } from '@/api/swagger/data-contracts';
 import { ref } from 'vue';
+
 import { useProduct } from '@/composables/useProduct';
+import { useApiMethod } from '@/api/useApiMethod';
+
+import type { ProductReturnDto } from '@/api/swagger/data-contracts';
+
+import { api } from '@/api/apiInstance';
 
 export const useLoadProductData = () => {
   const productRaw = ref<ProductReturnDto | null>(null);
-  const isLoading = ref(false);
+
+  const { call: getProduct, isLoading } = useApiMethod(api.getProduct);
 
   const product = useProduct(productRaw);
 
   const loadProduct = async (id: string) => {
-    try {
-      isLoading.value = true;
-      productRaw.value = await api.getProduct(id);
-    } catch (e: any) {
-      toast.error(e?.response?.data?.message || 'Error');
-    } finally {
-      isLoading.value = false;
-    }
+    productRaw.value = await getProduct(id);
   };
 
   return { product, isLoading, loadProduct };
