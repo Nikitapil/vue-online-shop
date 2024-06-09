866047200afa62497091e156d2b0b5605d7917d7
Nikitapil <nikitapiliavets@gmail.com>
2024-06-09 20:16:51 +0300

    add localization for useDiscountOptions composition

--- src/modules/product-page/components/SetupDiscountModal/useDiscountOptions.ts
+++ src/modules/product-page/components/SetupDiscountModal/useDiscountOptions.ts
@@ -7,15 +7,18 @@ import { useApiMethod } from '@/api/useApiMethod';
 
 import { api } from '@/api/apiInstance';
 import { NO_DISCOUNTS } from '@/domain/discounts';
+import { useI18n } from 'vue-i18n';
 
 export const useDiscountOptions = () => {
+  const { t } = useI18n();
+
   const discounts = ref<DiscountReturnDto[]>([]);
 
   const { isLoading, call: getDiscounts } = useApiMethod(api.getDiscounts);
 
   const options = computed<ISelectOptions[]>(() => {
     return [
-      { value: NO_DISCOUNTS, name: 'Without discount' },
+      { value: NO_DISCOUNTS, name: t('without_discount') },
       ...discounts.value.map((discount) => ({ value: discount.id, name: discount.name }))
     ];
   });
