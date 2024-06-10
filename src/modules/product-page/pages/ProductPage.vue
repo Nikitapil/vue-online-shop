22794b7e1d2b40abe7a56faadadfa97e0308c10e
Nikitapil <nikitapiliavets@gmail.com>
2024-06-10 23:52:25 +0300

    refactor ProductPage

--- src/modules/product-page/pages/ProductPage.vue
+++ src/modules/product-page/pages/ProductPage.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
-import ProductReviewsList from '../components/ProductReviewsList/ProductReviewsList.vue';
-import { useProductPageStore } from '@/modules/product-page/useProductPageStore';
 import { computed, onMounted, ref } from 'vue';
-import ProductDetails from '@/components/products/ProductDetails.vue';
-import RoundedLoader from '@/components/ui/loaders/RoundedLoader.vue';
-import EmptyStateCentered from '@/components/ui/EmptyStateCentered.vue';
-import { useProduct } from '@/composables/useProduct';
-import { storeToRefs } from 'pinia';
-import IconButton from '@/components/ui/IconButton.vue';
-import ConfirmModal from '@/components/ui/ConfirmModal.vue';
-import { ERoutesName } from '@/router';
 import { useRouter } from 'vue-router';
+import { storeToRefs } from 'pinia';
+
+import { useProductPageStore } from '@/modules/product-page/useProductPageStore';
+import { useProduct } from '@/composables/useProduct';
+
+import { ERoutesName } from '@/router';
+import { NO_DISCOUNTS } from '@/domain/discounts';
+
+import ConfirmModal from '@/components/ui/ConfirmModal.vue';
+import IconButton from '@/components/ui/IconButton.vue';
+import EmptyStateCentered from '@/components/ui/EmptyStateCentered.vue';
+import RoundedLoader from '@/components/ui/loaders/RoundedLoader.vue';
+import ProductDetails from '@/components/products/ProductDetails.vue';
 import ProductNotFound from '@/components/products/ProductNotFound.vue';
 import ToggleFavouritesButton from '@/widgets/ToggleFavouriteButton/ToggleFavouritesButton.vue';
 import AddToCart from '@/modules/cart/components/AddToCart.vue';
 import AppButton from '@/components/ui/AppButton.vue';
 import SetupDiscountModal from '@/modules/product-page/components/SetupDiscountModal/SetupDiscountModal.vue';
-import { NO_DISCOUNTS } from '@/domain/discounts';
+import ProductReviewsList from '../components/ProductReviewsList/ProductReviewsList.vue';
 
 const router = useRouter();
 
@@ -52,9 +55,9 @@ onMounted(() => {
       <RoundedLoader />
     </EmptyStateCentered>
 
-    <ProductNotFound v-else-if="!store.product" />
+    <ProductNotFound v-else-if="!product" />
 
-    <div v-else-if="product">
+    <div v-else>
       <div class="flex w-full justify-end gap-3">
         <AddToCart
           v-slot="{ clickHandler }"
@@ -65,25 +68,29 @@ onMounted(() => {
             appearance="secondary"
             @click="clickHandler"
           >
-            Add to cart +
+            {{ $t('add_to_cart') }} +
           </AppButton>
         </AddToCart>
+
         <AppButton
           v-if="product.canEditProductDiscount"
           @click="isSetupDiscountModalOpened = true"
         >
-          Setup discount
+          {{ $t('setup_discount') }}
         </AppButton>
+
         <ToggleFavouritesButton
           v-if="productFromStore?.canAddToFavourites"
           v-model="productFromStore"
         />
+
         <IconButton
           v-if="product.canEdit"
           :icon="'bx:edit'"
           color="#918aa0"
-          @click="router.push({ name: ERoutesName.EDIT_PRODUCT, params: { id: store.product.id } })"
+          @click="router.push({ name: ERoutesName.EDIT_PRODUCT, params: { id: product.id } })"
         />
+
         <IconButton
           v-if="product.canDelete"
           icon="bi:trash-fill"
@@ -91,20 +98,24 @@ onMounted(() => {
           @click="isDeleteModalOpened = true"
         />
       </div>
+
       <ProductDetails
         class="mb-5"
         :product="product"
       />
+
       <ProductReviewsList
         :product-id="product.id"
         :can-add-review="product.canAddReview"
       />
+
       <ConfirmModal
         v-model="isDeleteModalOpened"
-        title="Do you want to delete this product?"
+        :title="$t('delete_product_question')"
         :is-loading="store.isDeleteInProgress"
         @confirm="onDeleteProduct"
       />
+
       <SetupDiscountModal
         v-if="isSetupDiscountModalOpened"
         v-model="isSetupDiscountModalOpened"
