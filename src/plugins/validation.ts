61f51d4d95fe26bdd7119b5e51c0e5f3b8d828a0
Nikitapil <nikitapiliavets@gmail.com>
2024-06-12 13:09:26 +0300

    refactor validation plugin

--- src/plugins/validation.ts
+++ src/plugins/validation.ts
@@ -1,17 +1,19 @@
-import { defineRule } from 'vee-validate';
 import { capitalize } from 'vue';
+import { defineRule } from 'vee-validate';
+
+import { i18n } from '@/plugins/i18n';
 
 export const validation = () => {
   defineRule('required', (value: string, params, ctx) => {
     if (!value || !value.length) {
-      return `${capitalize(ctx.name) || 'This field'} is required`;
+      return `${capitalize(ctx.name) || i18n.global.t('this_field')} ${i18n.global.t('is_required')}`;
     }
     return true;
   });
 
   defineRule('requiredObj', (value: string, params, ctx) => {
     if (!value) {
-      return `${capitalize(ctx.name) || 'This field'} is required`;
+      return `${capitalize(ctx.name) || i18n.global.t('this_field')} ${i18n.global.t('is_required')}`;
     }
     return true;
   });
@@ -25,14 +27,14 @@ export const validation = () => {
         value
       )
     ) {
-      return 'Invalid email value';
+      return i18n.global.t('invalid_email_value');
     }
     return true;
   });
 
   defineRule('minLength', (value: string, [param]: [string], ctx) => {
     if (value.length < +param) {
-      return `${capitalize(ctx.name) || 'This field'} min length is ${param}`;
+      return `${capitalize(ctx.name) || i18n.global.t('this_field')} ${i18n.global.t('min_length_is')} ${param}`;
     }
     return true;
   });
