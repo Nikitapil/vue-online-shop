9043e65eec435b8b8b2308f46609364c9196ccba
Nikitapil <nikitapiliavets@gmail.com>
2024-06-12 12:42:54 +0300

    refactor ProfilePage

--- src/modules/profile/pages/ProfilePage.vue
+++ src/modules/profile/pages/ProfilePage.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+
+import { useAuthStore } from '@/modules/auth/authStore';
+
+import type { ChangePasswordDto, UpdateUserDataDto } from '@/api/swagger/data-contracts';
+
 import ChangePasswordModal from '../components/ChangePasswordModal.vue';
 import AppButton from '../../../components/ui/AppButton.vue';
 import AuthProtected from '@/modules/auth/components/AuthProtected.vue';
 import EditableText from '../../../components/ui/EditableText.vue';
-import { useAuthStore } from '@/modules/auth/authStore';
-import type { ChangePasswordDto, UpdateUserDataDto } from '@/api/swagger/data-contracts';
-import { computed, ref } from 'vue';
 
 const authStore = useAuthStore();
 
@@ -63,7 +66,8 @@ const onChangePassword = (request: ChangePasswordDto) => {
 <template>
   <AuthProtected>
     <div v-if="user">
-      <h2 class="text-2xl text-center mb-3">Profile</h2>
+      <h2 class="text-2xl text-center mb-3">{{ $t('profile') }}</h2>
+
       <EditableText
         id="email"
         v-model="isEmailEditMode"
@@ -74,40 +78,45 @@ const onChangePassword = (request: ChangePasswordDto) => {
         :initial-value="user.email"
         @submit="onUpdateEmail"
       />
+
       <EditableText
         id="name"
         v-model="isNameEditMode"
         class="mb-2"
         name="name"
-        label="Name:"
         rules="required"
+        :label="`${$t('name')}:`"
         :initial-value="user.name"
         @submit="onUpdateName"
       />
+
       <EditableText
         id="phone"
         v-model="isPhoneEditMode"
         class="mb-2"
         name="phone"
-        label="Phone:"
+        :label="`${$t('phone')}:`"
         :initial-value="userPhone"
         @submit="onUpdatePhone"
       />
+
       <EditableText
         id="address"
         v-model="isAddressEditMode"
         class="mb-2"
         name="address"
-        label="Address:"
+        :label="`${$t('address')}:`"
         :initial-value="userAddress"
         @submit="onUpdateAdress"
       />
+
       <AppButton
         appearance="secondary"
         @click="isShowChangePasswordModal = true"
       >
-        Change Password
+        {{ $t('change_password') }}
       </AppButton>
+
       <ChangePasswordModal
         v-model="isShowChangePasswordModal"
         :is-loading="authStore.isAuthLoading"
