0c308a90d41769b7dfc1aff7f0af2902dd49cacf
Nikitapil <nikitapiliavets@gmail.com>
2024-06-12 12:49:10 +0300

    refactor i18n imports orders

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,6 +1,9 @@
 import { createI18n } from 'vue-i18n';
+
 import { EAvailableTextLocales } from '@/domain/i18n.domain';
+
 import { getLocaleFromLocalStorage } from '@/helpers/localStorage-helpers';
+
 import messages from '../locales/index';
 
 export const i18n = createI18n({
