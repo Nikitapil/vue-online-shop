d7e3bf260dd42fa5ef45a9c56ce54d3b5d3f452b
Nikitapil <nikitapiliavets@gmail.com>
2024-06-14 21:44:17 +0300

    refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import './assets/main.css';
-import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
 
 import { createApp } from 'vue';
-import App from './modules/app/pages/App.vue';
-import { router } from '@/router';
 import { createPinia } from 'pinia';
+
 import { validation } from '@/plugins/validation';
+import { router } from '@/router';
 import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
 import { DirectivePlugin } from '@/directives/directive-plugin';
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
 import { i18n } from '@/plugins/i18n';
+
+import 'vue3-toastify/dist/index.css';
+
+import App from './modules/app/pages/App.vue';
 
 const pinia = createPinia();
 const app = createApp(App);
